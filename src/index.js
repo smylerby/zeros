9e4487707e784526ab7f82fb636d04a74699f169
Rustam Shorov <srmparusa@gmail.com>
2018-02-27 20:09:26 +0100

    Fertig!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function getZerosCount(number){
+    let nulls = 0;
+    let exponent = 5;
+    while(exponent < number){
+        nulls += Math.floor(number/exponent);
+        exponent *= 5;
+    }
+    return nulls;
 }
